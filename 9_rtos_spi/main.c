9997b64bb218266d7bb1e24ba7076f88fb36b362
Jakob Kastelic <jkastelic@thinksrs.com>
2023-09-29 10:40:23 -0700

    implement toggling conf_reg bits

--- 9_rtos_spi/main.c
+++ 9_rtos_spi/main.c
@@ -21,10 +21,14 @@
 #define I2S_CHANNEL 0x02
 #define PRBS_ENABLE 0x04
 #define BLANK_LEVEL 0x08
-#define COUNTER_WIDTH0 0x10
-#define COUNTER_WIDTH1 0x20
-#define COUNTER_WIDTH2 0x40
-#define COUNTER_WIDTH3 0x80
+#define CNT_SEL0 0x10
+#define CNT_SEL1 0x20
+#define CNT_SEL2 0x40
+#define CNT_SEL3 0x80
+#define CNT_SEL4 0x100
+
+// state variables
+uint16_t conf_reg = 0x0000;
 
 void setup_MCU(void)
 {
@@ -58,15 +62,38 @@ void vTask1(void *pvParameters)
             break;
 
          case '0' : // clear FPGA configuration register
-            transmit_SPI(0x80, 0x00, 0x00);
+            conf_reg = 0x0000;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
             break;
 
-         case 'p' : // enable PRBS
-            transmit_SPI(0x80, 0x00, PRBS_ENABLE);
+         case 'p' : // toggle PRBS on/off
+            conf_reg ^= PRBS_ENABLE;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
             break;
 
-         case 's' : // enable slow counter
-            transmit_SPI(0x80, 0x00, COUNTER_WIDTH0);
+         case 's' : // toggle div-by-2 counter ("slow mode")
+            conf_reg ^= CNT_SEL0;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
+            break;
+
+         case '1' : // toggle decimal counter (bit 1)
+            conf_reg ^= CNT_SEL1;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
+            break;
+
+         case '2' : // toggle decimal counter (bit 2)
+            conf_reg ^= CNT_SEL2;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
+            break;
+
+         case '3' : // toggle decimal counter (bit 3)
+            conf_reg ^= CNT_SEL3;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
+            break;
+
+         case '4' : // toggle decimal counter (bit 4)
+            conf_reg ^= CNT_SEL4;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
             break;
 
          case 'F' : // enable FSK SPI FAST
@@ -78,12 +105,14 @@ void vTask1(void *pvParameters)
             transmit_SPI(0x7C, 0x00, 0x00);             // LMX: FSK_SPI_FAST_DEV
             break;
 
-         case 'l' : // echo I2S (left channel) to SPI
-            transmit_SPI(0x80, 0x00, ECHO_I2S_TO_SPI);
+         case 'i' : // toggle echoing I2S to SPI
+            conf_reg ^= ECHO_I2S_TO_SPI;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
             break;
 
-         case 'r' : // echo I2S (right channel) to SPI
-            transmit_SPI(0x80, 0x00, ECHO_I2S_TO_SPI | I2S_CHANNEL);
+         case 'c' : // toggle I2S channel (left or right)
+            conf_reg ^= I2S_CHANNEL;
+            transmit_SPI(0x80, conf_reg>>8, conf_reg);
             break;
       }
    }
